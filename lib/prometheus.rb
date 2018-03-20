09953f1bbe68edd88f6cb15996b054cc44ed3075
André Bauer <monotek23@gmail.com>
2018-03-20 17:08:29 +0100

    prometheus tests

--- lib/prometheus.rb
+++ lib/prometheus.rb
@@ -1,20 +1,18 @@
 require 'prometheus/client'
 
 module Prometheus
-  module Controller
     # returns a default registry
     prometheus = Prometheus::Client.registry
 
     # create a new counter metric
-    http_requests = Prometheus::Client::Counter.new(:http_requests, 'A counter of HTTP requests made')
+    #http_requests = Prometheus::Client::Counter.new(:http_requests, 'A counter of HTTP requests made')
 
     # register the metric
     prometheus.register(http_requests)
 
     # equivalent helper function
-    #http_requests = prometheus.counter(:http_requests, 'A counter of HTTP requests made')
+    http_requests = prometheus.counter(:http_requests, 'A counter of HTTP requests made')
 
     # start using the counter
     http_requests.increment
-  end
 end
